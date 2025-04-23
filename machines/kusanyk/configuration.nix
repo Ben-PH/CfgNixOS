75d9cc8a9f79690adc8e246615d100d0304a1451
Ben-PH <benphawke@gmail.com>
2025-04-23 12:22:56 +0200

    minor kusanyk correction

--- machines/kusanyk/configuration.nix
+++ machines/kusanyk/configuration.nix
@@ -17,11 +17,12 @@
     ../common/core.nix
     ../common/environment.nix
     ../common/boot.nix
+    ./hardware-configuration.nix
   ];
 
   networking.hostName = "kusanyk";
 
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
